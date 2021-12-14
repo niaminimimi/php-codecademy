c38efd55c8a6efd0c1e27675f21532216db5bcac
niaminimimi <chellychiru@gmail.com>
2021-12-14 15:24:22 +0300

    qns php

---
+++ w3resource/qn1.php
@@ -0,0 +1,5 @@
+<?php
+$color = array('white', 'green', 'red', 'blue', 'black');
+
+echo "The memory of that scene for me is like a frame of film forever frozen at that moment: the red carpet, the green lawn, the white house, the leaden sky. The new president and his first lady. - Richard M. Nixon";
+echo implode(",",$color);
---
+++ w3resource/qn2.php
@@ -0,0 +1,23 @@
+<?php
+$color = array('white', 'green', 'red');
+echo implode(",",$color);
+echo "</br>";
+echo "<ul>";
+
+echo "<li>";
+echo $color[0];
+echo "</li>";
+echo "</br>";
+
+echo "<li>";
+echo $color[1];
+echo "</li>";
+echo "</br>";
+
+echo "<li>";
+echo $color[2];
+echo "</li>";
+echo "</br>";
+
+echo "</ul>";
+?>
